b8b8d9f6962875b72eb4ed6164187bb63d6f7a70
Joonas Lehtonen <lehton83@gmail.com>
2016-07-26 11:19:57 +0300

    fix copy to clipboard not working on firefox

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import 'jquery';
+import $ from 'jquery';
 import 'bootstrap-sass';
+
+/* Fixes clipboard.js not working with Bootstrap modals on some browsers (mainly FF).
+More info: https://github.com/zenorocha/clipboard.js/issues/155
+*/
+$.fn.modal.Constructor.prototype.enforceFocus = function() {};
+
 import Vue from 'vue';
 
 import { clamp, duration } from './filters';
